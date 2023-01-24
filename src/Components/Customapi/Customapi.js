7e2e4cd78279cd0843603e948d6fda269033ed49
asim <asim.maknojia1997@gmail.com>
2023-01-24 19:32:52 +0500

    updatesss

--- src/Components/Customapi/Customapi.js
+++ src/Components/Customapi/Customapi.js
@@ -1,14 +1,38 @@
-import React, { Component } from 'react';
+import React, {useEffect, useState} from 'react';
 import './Customapi.css';
 
-class Customapi extends Component {
-render(){
-return(
-    <div className=''>
+function Customapi(){
+// return(
+//     <div className=''>
+        
+//     </div>    ) 
+const [user, setUser] = useState([]);
 
-    </div>    )   
+  const fetchData = () => {
+    return fetch("https://jsonplaceholder.typicode.com/users")
+          .then((response) => response.json())
+          .then((data) => setUser(data));
+  }
+
+  useEffect(() => {
+    fetchData();
+  },[])
+
+  return (
+    <main>
+      <h1>User List</h1>
+      <ul>
+        {user && user.length > 0 && user.map((userObj, index) => (
+            <li key={userObj.id}>{userObj.name}</li>
+          ))}
+        {user && user.length > 0 && user.map((userObj, index) => (
+            <li key={userObj.id}>{userObj.email}</li>
+          ))}
+      </ul>
+    </main>
+  );  
 }
-}
+
 
 
     export default Customapi;
